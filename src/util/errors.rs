13de77e968edc7b3b6e834cc8dbb6788a198fcb0
antipoly <80296087+antipoly@users.noreply.github.com>
2025-08-14 18:56:10 -0500

    feat: fix (?) oxiderror

--- src/util/errors.rs
+++ src/util/errors.rs
@@ -29,6 +29,19 @@ pub enum OxidError {
   Validation(String),
 }
 
+impl std::fmt::Display for OxidError {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self {
+      OxidError::Api(errors) => write!(f, "OxidRoblox API Error: {:?}", errors),
+      OxidError::Http(e) => write!(f, "OxidRoblox HTTP Error: {}", e),
+      OxidError::Unauthorized => write!(f, "OxidRoblox Error: Invalid or unauthorized cookie when using an authenticated request"),
+      OxidError::Validation(msg) => write!(f, "OxidRoblox Rrror: {}", msg),
+    }
+  }
+}
+
+impl std::error::Error for OxidError {}
+
 /// parses the error json from a bad request and returns Ok(ApiErrors)
 ///
 pub(crate) async fn get_api_errors_from_response(response: Response) -> RobloxResult<ApiErrors> {
